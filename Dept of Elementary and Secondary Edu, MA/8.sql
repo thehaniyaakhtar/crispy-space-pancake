9f82442531b14e818246cace1cf844cf5eaabd34
haniya akhtar <thehaniyaakhtar@gmail.com>
2025-05-07 14:42:23 +0530

    Add 8.sql from DESE

---
+++ Dept of Elementary and Secondary Edu, MA/8.sql
@@ -0,0 +1,8 @@
+--A parent wants to send their child to a district with many other students.
+--In 8.sql, write a SQL query to display the names of all school districts and the number of pupils enrolled in each.
+SELECT districts.name,
+       expenditures.pupils
+FROM districts
+JOIN expenditures
+ON expenditures.district_id = districts.id
+;
