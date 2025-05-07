e1301068a9f9741101c05922d13b0fa673073a95
haniya akhtar <thehaniyaakhtar@gmail.com>
2025-05-07 13:27:19 +0530

    Adding 4.sql from MLB

---
+++ major league baseball/4.sql
@@ -0,0 +1,9 @@
+--ind the first and last names of players who were not born in the United States.
+--Sort the results alphabetically by first name, then by last name.
+SELECT first_name,
+       last_name
+FROM players
+WHERE birth_country != "USA"
+ORDER BY first_name, last_name
+;
+
