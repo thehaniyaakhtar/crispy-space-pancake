476aaeca781147cf068f5655844410b715203f23
haniya akhtar <thehaniyaakhtar@gmail.com>
2025-05-07 13:26:49 +0530

    Adding 1.sql from MLB

---
+++ major league baseball/1.sql
@@ -0,0 +1,7 @@
+--find the hometown (including city, state, and country) of Jackie Robinson.
+SELECT birth_city,
+       birth_state,
+       birth_country
+FROM players
+WHERE first_name = "Jackie" AND last_name = "Robinson"
+;
