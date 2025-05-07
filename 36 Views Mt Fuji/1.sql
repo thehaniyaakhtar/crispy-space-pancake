15b003301863ce3e16fe2d6260683e887a3e45f4
haniya akhtar <thehaniyaakhtar@gmail.com>
2025-05-07 14:25:22 +0530

    1.sql from views

---
+++ 36 Views Mt Fuji/1.sql
@@ -0,0 +1,6 @@
+--list, side by side, the Japanese title and the English title for each print.
+--Ensure the Japanese title is the first column, followed by the English title.
+SELECT japanese_title,
+       english_title
+FROM views
+;
