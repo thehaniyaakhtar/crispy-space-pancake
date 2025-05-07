2c07070574e0c94be5c102527cec93459a317249
haniya akhtar <thehaniyaakhtar@gmail.com>
2025-05-07 13:27:37 +0530

    Adding 6.sql from MLB

---
+++ major league baseball/6.sql
@@ -0,0 +1,9 @@
+-- return the first name, last name, and debut date of players born in Pittsburgh, Pennsylvania (PA).
+--Sort the results first by debut date—from most recent to oldest—then alphabetically by first name, followed by last name.
+SELECT first_name,
+       last_name,
+       debut
+FROM players
+WHERE birth_city = "Pittsburgh" and birth_state = "PA"
+ORDER BY debut DESC, first_name, last_name
+;
