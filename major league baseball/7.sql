5e71d6d059bf0ff64b23f04e6cf281fa36c98042
haniya akhtar <thehaniyaakhtar@gmail.com>
2025-05-07 13:27:46 +0530

    Adding 7.sql from MLB

---
+++ major league baseball/7.sql
@@ -0,0 +1,5 @@
+--count the number of players who bat (or batted) right-handed and throw (or threw) left-handed, or vice versa.
+SELECT COUNT(*) id
+FROM players
+WHERE (bats = "R" AND throws = "L") OR (bats = "L" AND throws = "R")
+;
