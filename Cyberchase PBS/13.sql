de78fdfca91c83d68689b82e496af954958bbae8
haniya akhtar <thehaniyaakhtar@gmail.com>
2025-05-07 14:49:40 +0530

    Cyberchase 13

---
+++ Cyberchase PBS/13.sql
@@ -0,0 +1,6 @@
+--write a SQL query to explore a question of your choice. This query should:
+--Involve at least one condition, using WHERE with AND or OR
+SELECT title, topic
+FROM episodes
+WHERE topic LIKE "%E%" AND (season = 3 OR season = 6)
+;
