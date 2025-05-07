849d40573ed096b7a13789f0f364a278e7ac090d
haniya akhtar <thehaniyaakhtar@gmail.com>
2025-05-07 10:10:59 +0530

    Add 9.sql from moneyball

---
+++ moneyball/9.sql
@@ -0,0 +1,12 @@
+--5 lowest paying teams (by average salary) in 2001.
+--Round the average salary column to two decimal places and call it “average salary”.
+--Sort the teams by average salary, least to greatest.
+--Your query should return a table with two columns, one for the teams’ names and one for their average salary.
+SELECT teams.name,
+       ROUND(AVG(salaries.salary), 2) AS "average salary"
+FROM teams
+JOIN salaries ON teams.id = salaries.team_id
+WHERE salaries.year = 2001
+GROUP BY teams.name
+ORDER BY "average salary" ASC
+LIMIT 5;
