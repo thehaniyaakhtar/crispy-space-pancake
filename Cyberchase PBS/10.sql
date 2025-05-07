5f178c74095e4a82a0fd012d221dc628de5b0ad4
haniya akhtar <thehaniyaakhtar@gmail.com>
2025-05-07 14:49:21 +0530

    Cyberchase 10

---
+++ Cyberchase PBS/10.sql
@@ -0,0 +1,8 @@
+--write a SQL query to list the ids, titles, and production codes of all episodes.
+--Order the results by production code, from earliest to latest.
+SELECT id,
+       title,
+       production_code
+FROM episodes
+ORDER BY production_code ASC
+;
