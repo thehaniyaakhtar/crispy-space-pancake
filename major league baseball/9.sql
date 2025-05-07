8bb340973fb438e0b961ec05f1fe0a283be3a63b
haniya akhtar <thehaniyaakhtar@gmail.com>
2025-05-07 13:28:15 +0530

    Adding 9.sql from MLB

---
+++ major league baseball/9.sql
@@ -0,0 +1,8 @@
+--players who played their final game in the MLB in 2022.
+--Sort the results alphabetically by first name, then by last name.
+SELECT first_name,
+       last_name
+FROM players
+WHERE final_game >= "2022-01-01" AND final_game <= "2022-12-31"
+ORDER BY first_name, last_name
+;
