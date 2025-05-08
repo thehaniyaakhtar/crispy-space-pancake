45d4293c0a2cca046b538af93e7ac4a0954cd0f1
haniya akhtar <thehaniyaakhtar@gmail.com>
2025-05-08 10:00:06 +0530

    10.sql

---
+++ 36 Views Mt Fuji/10.sql
@@ -0,0 +1,10 @@
+--In 10.sql, write a SQL query to answer a question of your choice about the prints. The query should:
+--Make use of AS to rename a column
+--Involve at least one condition, using WHERE
+--Sort by at least one column, using ORDER BY
+SELECT english_title,
+       contrast AS "Hokusai Contrast"
+FROM views
+WHERE artist = 'Hokusai'
+ORDER BY contrast DESC
+LIMIT 5;
