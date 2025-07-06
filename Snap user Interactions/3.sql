56fc6177386686851ff3481fb02c2a4d9a0d7473
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2025-07-06 10:52:37 +0530

    3.sql

---
+++ Snap user Interactions/3.sql
@@ -0,0 +1,8 @@
+/* Top 3 friends messaged by creativewisdom377 */
+SELECT to_user_id
+FROM messages
+WHERE from_user_id = (SELECT id FROM users WHERE username = 'creativewisdom377')
+GROUP BY to_user_id
+ORDER BY count(*) DESC
+LIMIT 3
+;
