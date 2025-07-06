d3715c0a061cfd589c5ef530c1cb64782d76ef6b
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2025-07-06 11:01:09 +0530

    june vacancies

---
+++ BnB/june_vacancies.sql
@@ -0,0 +1,12 @@
+CREATE VIEW june_vacancies AS
+SELECT listings.id,
+       listings.property_type,
+       listings.host_name,
+       COUNT(availabilities.id) AS days_vacant
+FROM listings
+JOIN availabilities ON listings.id = availabilities.listing_id
+WHERE availabilities.available = 'TRUE'
+  AND availabilities.date >= '2023-06-01'
+  AND availabilities.date <= '2023-06-30'
+GROUP BY listings.id
+;
